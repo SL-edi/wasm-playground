cb613393c0b8070518eb725ffc135b2676ffff39
Tadeusz Janowski <tadjanow@gmail.com>
2020-10-11 10:47:49 +0100

    Rewrote RK4 using wikipedia article, runs much faster now

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,7 +1,4 @@
 mod utils;
-
-extern crate peroxide;
-use peroxide::fuga::*;
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -11,21 +8,19 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-pub fn integrate_exp(t0: f64, y0: f64, tfinal: f64, n_steps: usize) -> f64 {
-    let init_state = State::<f64>::new(t0, vec!(y0), vec!(-y0));
-    let mut solver = ExplicitODE::new(exp_function);
-    let dt = (tfinal - t0)/(n_steps as f64);
-    solver
-        .set_method(ExMethod::RK4)
-        .set_initial_condition(init_state)
-        .set_step_size(dt)
-        .set_times(n_steps);
-    solver.integrate().data.pop().unwrap()
+pub fn integrate_exp(t0: f64, y0: f64, t_final: f64, n_steps: usize) -> f64 {
+    let dt = (t_final - t0)/(n_steps as f64);
+    let mut current_y = y0;
+    for _ in 0..n_steps {
+        current_y = rk4_step(t0, dt, current_y, |_,y|{-y});
+    }
+    current_y
 }
 
-fn exp_function(state: &mut State<f64>, _: &NoEnv) {
-    let y = &state.value;
-    let dy = &mut state.deriv;
-
-    dy[0] = -y[0];
+fn rk4_step(t: f64, dt: f64, y0: f64, f: impl Fn(f64, f64) -> f64) -> f64 {
+    let k1 = f(t, y0);
+    let k2 = f(t + dt/2.0, y0 + dt*k1/2.0);
+    let k3 = f(t + dt/2.0, y0 + dt*k2/2.0);
+    let k4 = f(t + dt, y0 + dt*k3);
+    y0 + dt/6.0*(k1 + 2.0*k2 + 2.0*k3 + k4)
 }
